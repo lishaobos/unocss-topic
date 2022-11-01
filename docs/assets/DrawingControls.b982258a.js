ca86d77093ae4783c148d06fb7c1134a6fee13c9
cuiluc <cuilulu4043@126.com>
2022-11-01 16:28:22 +0800

    fix: 打包恢复到 dist

--- docs/assets/DrawingControls.b982258a.js
+++
@@ -1,1 +0,0 @@
-import"./DrawingControls.vue_vue_type_script_setup_true_lang.7592f0f0.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.7592f0f0.js";import"./index.1e9f3597.js";export{t as default};
--- docs/assets/DrawingPreview.53726ae0.js
+++
@@ -1,1 +0,0 @@
-import"./index.1e9f3597.js";import{al as r}from"./index.1e9f3597.js";export{r as default};
